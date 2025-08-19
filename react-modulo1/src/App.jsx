8916534ba702b933f7e43fb4b2af9ebfb428b500
Catalina <cp131537@gmail.com>
2025-08-18 23:34:53 -0500

    subiendo

--- react-modulo1/src/App.jsx
+++ react-modulo1/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
     <div>
       <Navbar />
 
-      {/* Contenido principal */}
+      
       <main className="p-8 text-center">
         <h1 className="text-4xl font-bold mb-4">Bienvenido a Mi App</h1>
         <p className="text-lg text-gray-600">
